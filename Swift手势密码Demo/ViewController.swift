739cdbda79ddb74a730e89c22bb3b820d155fb40
韩磊 <hanlei@hanleideMacBook-Pro.local>
2017-12-09 11:27:32 +0800

    first commit

--- Swift手势密码Demo/ViewController.swift
+++ Swift手势密码Demo/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    ///创建手势密码
+    @IBAction func creatGesturepasswordButtonAction(_ sender: UIButton) {
+        let creatGesturepasswordVC = HLGestureViewController()
+        self.present(creatGesturepasswordVC, animated: true, completion: nil)
+        
     }
-
-
+    ///验证手势密码
+    @IBAction func vailteGeturepasswordButtonAction(_ sender: UIButton) {
+        if UserDefaults.standard.string(forKey: gesturepasswordKey)?.isEmpty == true {
+            print("请先创建手势密码")
+            return
+        }
+        //验证手势密码
+        let vaildGesturepasswordVC = HLValidateGestureViewController()
+        self.present(vaildGesturepasswordVC, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
